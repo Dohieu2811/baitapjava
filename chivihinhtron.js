e3959051d9967cbd10534d9d7272290f7bd58675
Dohieu2811 <146150057+Dohieu2811@users.noreply.github.com>
2023-11-28 13:26:24 +0700

    chivihinhtron.js

--- chivihinhtron.js
+++ chivihinhtron.js
@@ -4,7 +4,7 @@
             var duongKinh = 2 * banKinh;
             var chuVi = 2 * banKinh * 3.14;
             var dienTich = banKinh * banKinh * 3.14;
-            document.getElementById("chuViResult").textContent = "Chu vi của hình tròn là: " + chuVi;
-            document.getElementById("dienTichResult").textContent = "Diện tích của hình tròn là: " + dienTich;
+            document.getElementById("ketquachuvi").textContent = "Chu vi của hình tròn là: " + chuVi;
+            document.getElementById("ketquadientich").textContent = "Diện tích của hình tròn là: " + dienTich;
         }
     
